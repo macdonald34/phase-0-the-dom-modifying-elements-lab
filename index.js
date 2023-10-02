f6726249d2c382cef2bdec10ffa4024898652bc2
macdonald34 <144123866+macdonald34@users.noreply.github.com>
2023-10-02 12:05:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code here!
+// Write your code here!
 const main = document.getElementById('main')
 main.remove();
 
@@ -6,5 +7,12 @@ const body = document.body
 
 const newHeader = document.createElement ('h1')
 newHeader.id = 'victory'
-newHeader.innerHTML = Macdonald is the champion'
+newHeader.innerHTML = 'Macdonald Muhavi is the champion'
 body.appendChild(h1);
+
+
+
+
+
+
+
